4cf965be8c3a57003f9264f75ac03de52b63b36c
Hb-key <44340302+Hb-key@users.noreply.github.com>
2018-12-03 17:09:51 +0800

    Add files via upload

---
+++ Link List.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<assert.h>
+#include<stdio.h>
+
+typedef int DataType;
+
+typedef struct ListNode {
+	int data;
+	struct ListNode *next;
+}ListNode;
+
+//从尾到头打印单链表
+void TailToHead(ListNode *plist)
+{
+	assert(plist);
+	if (plist == NULL)
+	{
+		return;
+	}
+	TailToHead(plist->next);
+	printf("%d", plist->data);
+}
+
+//删除一个无头单链表的非尾节点
+void DeleteNoHead(ListNode *pos)
+{
+	assert(pos);
+	ListNode *cur = pos;
+	ListNode *next = pos->next;
+	cur->data = next->data;
+	cur->next = next->next;
+
+}
