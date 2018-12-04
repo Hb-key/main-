3ce5fd4b1e0c45cb1047bed2f0f38e6a139a4f99
Hb-key <44340302+Hb-key@users.noreply.github.com>
2018-12-04 12:16:51 +0800

    Add files via upload

---
+++ 进程调度.c
@@ -0,0 +1,57 @@
+#include"进程调度.h"
+
+//void menu()
+//{
+//	printf("************************\n");
+//	printf("****  1.先来先服务  ****\n");
+//	printf("****  2.短作业优先  ****\n");
+//	printf("****  3.优先级调度  ****\n");
+//	printf("****  4.时间片轮转  ****\n");
+//	printf("****  0.退出        ****\n");
+//	printf("************************\n");
+//}
+//
+//void Logic()
+//{
+//	menu();
+//	int input;
+//	printf("请选择你想使用的进程调度的方式：");
+//	scanf("%d", &input);
+//	switch (input)
+//	{
+//	case 0:
+//		printf("退出！\n");
+//		break;
+//	case 1:
+//		printf("先来先服务算法模拟:\n");
+//		GetInfo();
+//		p = head;
+//		FCFS();
+//		break;
+//	case 2:
+//		printf("短作业优先算法模拟:\n");
+//		GetInfo();
+//		break;
+//	case 3:
+//		printf("优先级调度算法模拟:\n");
+//		GetInfo();
+//		break;
+//	case 4:
+//		printf("时间片轮转算法模拟:\n");
+//		GetInfo();
+//		break;
+//	default:
+//		printf("输入错误！！\n");
+//		break;
+//	}
+//}
+
+
+int main()
+{
+	printf("先来先服务算法模拟:\n");
+	GetInfo();
+	p = head;
+	FCFS();
+	return 0;
+}
--- 进程调度.h
+++ 进程调度.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-//#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS 
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -27,10 +27,30 @@ pcb *head = NULL;//进程链表指针
 pcb *p = NULL;
 pcb *q = NULL;
 
+
+void Run_FCFS(pcb *p1)   //运行未完成的进程
+{
+	int time = 0;		 //计时器
+	time = p1->ArriveTime> time ? p1->ArriveTime : time;
+	p1->StartTime = time;
+	time += p1->ServiceTime;
+	p1->state = 'T';
+	p1->FinishTime = time;
+
+	p1->WholeTime = (float)p1->FinishTime - (float)p1->ArriveTime;
+	p1->WeightWholeTime = p1->WholeTime / p1->ServiceTime;
+	x += p1->WholeTime;
+	y += p1->WeightWholeTime;
+	p1->AverageWT = p1->WholeTime / n;
+	p1->AverageWWT = p1->WeightWholeTime / n;
+
+	printf("%4s %6d %8d %8d %8d %9.2f %10.2f %12.2f %12.2f\n",
+		p1->name, p1->ArriveTime, p1->StartTime, p1->ServiceTime, p1->FinishTime,
+		p1->WholeTime, p1->WeightWholeTime, p1->AverageWT, p1->AverageWWT);
+}
 void FCFS()
 {
-	p = head;
-	printf("作业名 到达时间 开始时间 服务时间 完成时间 周转时间 带权周转时间 平均周转时间 平均带全周转时间\n");
+	printf("作业名 到达时间 开始时间 服务时间 完成时间 周转时间 带权周转时间 平均周转时间 平均带权周转时间\n");
 	printf("----------------------------------------------------------------------------------------------\n");
 	for (int i = 0; i<n; i++)
 	{
@@ -43,25 +63,85 @@ void FCFS()
 	}
 }
 
-void Run_FCFS(pcb *p1)   //运行未完成的进程
-{
-	int time = 0;		 //计时器
-	time = p1->ArriveTime> time  ? p1->ArriveTime : time;
-	p1->StartTime = time;
-	time += p1->ServiceTime;
-	p1->state = 'T';
-	p1->FinishTime = time;
-	p1->WholeTime = p1->FinishTime - p1->ArriveTime;
-	p1->WeightWholeTime = p1->WholeTime / p1->ServiceTime;
-	x += p1->WholeTime;
-	y += p1->WeightWholeTime;
-	p1->AverageWT = p1->WholeTime / n;
-	p1->AverageWWT = p1->WeightWholeTime / n;
-	printf("%4s %6d %8d %8d %8d %9.2f %10.2f %12.2f %12.2f\n",
-		p1->name, p1->ArriveTime, p1->StartTime, p1->ServiceTime, p1->FinishTime,
-		p1->WholeTime, p1->WeightWholeTime, p1->AverageWT, p1->AverageWWT);
-}
 
+void Psort(pcb *p2, int N)
+{
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (p2[i].ArriveTime < p2[j].ArriveTime)
+			{
+				pcb Temp = p2[i];
+				p2[i] = p2[j];
+				p2[j] = Temp;
+			}
+		}
+	}
+	
+	for (int k = 0; k < N - 1; k++)
+	{
+		if (k == 0)
+		{
+			p2[k].StartTime = p2[k].ArriveTime;
+			p2[k].FinishTime = p2[k].ArriveTime + p2[k].ServiceTime;
+		}
+		else
+		{
+			p2[k].StartTime = p2[k - 1].FinishTime;
+			p2[k].FinishTime = p2[k].StartTime + p2[k].ServiceTime;
+		}
+		//选择最短作业执行
+		int m = 0;
+		for (int t = k + 1; t <= N - 1; t++)
+		{
+			if (p2[t].ArriveTime <= p2[k].FinishTime)
+				m++;
+		}
+		float min = p2[k + 1].ServiceTime;
+		int next = k + 1;
+		for (int q = k + 1; q < k + m; q++)
+		{
+			if (p2[q + 1].ServiceTime < min)
+			{
+				min = p2[q + 1].ServiceTime;
+				next = q + 1;
+			}
+			pcb Temp = p2[k + 1];
+			p2[k + 1] = p2[next];
+			p2[next] = Temp;
+		}
+	}
+}
+void Pdeal(pcb *p2, float ArriveTime, float ServiceTime, float StartTime, 
+	float FinishTime, float &WholeTime, float&WeightWholeTime, int N)
+{
+	for (int k = 0; k < N; k++)
+	{
+		if (k == 0)
+		{
+			p[k].StartTime = p[k].ArriveTime;
+			p[k].FinishTime = p[k].ArriveTime + p[k].ServiceTime;
+		}
+		else
+		{
+			p[k].StartTime = p[k - 1].FinishTime
+				p[k].FinishTime = p[k].StartTime + p[k].ServiceTime;
+		}
+	}
+	for (int k = 0; k<N; k++)
+	{
+		p[k].WholeTime = p[k].FinishTime - p[k].ArriveTime;
+		p[k].WeightWholeTime = p[k].WholeTime / p[k].ServiceTime;
+	}
+
+}
+void SJF(pcb *p2, int N)
+{
+	Psort(p2, N);
+	Pdeal(p2, ArriveTime, ServiceTime, StartTime, FinishTime, WholeTime, WeightWholeTime, N);
+	Poutput(p2, N);
+}
 
 void GetInfo()
 {
@@ -94,3 +174,6 @@ void GetInfo()
 		q = p;
 	}
 }
+
+//ABC　０１５　３１２
+//ABC　００３　２２２
