8639281dcb4fc5808c3872821e8b47fdca592aeb
Hb-key <44340302+Hb-key@users.noreply.github.com>
2019-01-04 18:18:27 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,27 +1,66 @@
-#include"CommentConvert.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include"Stu_ach_manage.h"
 
-void test()
+void menu()
 {
-	FILE* pRead = fopen("input.c", "r");
-	if (pRead == NULL)
-	{
-		perror("error for pRead");
-		exit(EXIT_FAILURE);
-	}
-	FILE* pWrite = fopen("output.c", "w");
-	if (pWrite == NULL)
-	{
-		perror("error for pWrite");
-		exit(EXIT_FAILURE);
-	}
-	CommentConvert(pRead, pWrite);
-	fclose(pRead);
-	pRead = NULL;
-	fclose(pWrite);
-	pWrite = NULL;
+	printf("***********************************************\n");
+	printf("**********      学生成绩管理系统      *********\n");
+	printf("***********************************************\n");
+	printf("**********     1、录入学生信息系统    *********\n");
+	printf("**********     2、删除学生信息系统    *********\n");
+	printf("**********     3、修改学生信息系统    *********\n");
+	printf("**********     4、查询学生信息系统    *********\n");
+	printf("**********     5、输出学生信息系统    *********\n");
+	printf("**********     6、退出系统            *********\n");
+	printf("***********************************************\n");
 }
+
+void Logic(struct student *s)
+{
+	while (1)
+	{
+		menu();
+		printf("请选择：");
+		int input = 0;
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			Entry(s);
+			break;
+		case 2:
+			Delete(s);
+			break;
+		case 3:
+			Modify(s);
+			break;
+		case 4:
+			Search(s);
+			break;
+		case 5:
+			Print(s);
+			break;
+		case 6:
+			printf("退出系统！！！\n");
+			break;
+		default:
+			printf("输入有误，请重新输入！");
+			break;
+		}
+	}
+}
+
 int main()
 {
-	test();
-	return  0;
+	
+	struct student s[info] = {
+		{ "10001","zhang","Chinese",85,"male" },
+	{ "10002","wang","C",83,"female" },
+	{ "10003","liang","Chemist",74,"male" },
+	{ "10004","dong","English",78,"female" },
+	{ "10005","meng","Math",87,"male" }
+	};
+
+	Logic(s);
+	return 0;
 }
